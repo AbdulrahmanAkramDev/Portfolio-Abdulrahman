dee6836ed957dffdf784a5c80f433ff253be2782
Abdulrahman <abdulrahmanakram.01@gmail.com>
2025-07-04 18:56:25 +0300

    final fix before redeploy

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,25 +1,25 @@
-import ProjectPortfolio from "./ProjectPortfolio.png";
-import WeatherApp from "./WeatherApp.png";
-import FoodDeliveryApp from "./FoodDeliveryApp.png";
+import projectportfolio from "./projectportfolio.png";
+import weatherapp from "./weatherapp.png";
+import fooddeliveryapp from "./fooddeliveryapp.png";
 
 export const PROJECTS = [
   {
     title: "Portfolio Website",
-    image: ProjectPortfolio,
+    image: projectportfolio,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
     technologies: ["React", "Tailwind CSS", "Framer Motion", "Vite"],
   },
   {
     title: "Weather App",
-    image: WeatherApp,
+    image: weatherapp,
     description:
       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
     technologies: ["React", "CSS"],
   },
   {
     title: "food Delivery Website",
-    image: FoodDeliveryApp,
+    image: fooddeliveryapp,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
     technologies: ["React", "CSS", "Vite"],
