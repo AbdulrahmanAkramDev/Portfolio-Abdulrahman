ffc6f1a659509ff7c9e085224774db2f56a02095
Abdulrahman <abdulrahmanakram.01@gmail.com>
2025-07-04 18:00:42 +0300

    p commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,13 +4,6 @@ import FoodDeliveryApp from "./FoodDeliveryApp.png";
 
 export const PROJECTS = [
   {
-    title: "food Delivery Website",
-    image: FoodDeliveryApp,
-    description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["React", "CSS", "Vite"],
-  },
-  {
     title: "Portfolio Website",
     image: ProjectPortfolio,
     description:
@@ -23,5 +16,12 @@ export const PROJECTS = [
     description:
       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
     technologies: ["React", "CSS"],
+  },
+  {
+    title: "food Delivery Website",
+    image: FoodDeliveryApp,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["React", "CSS", "Vite"],
   },
 ];
